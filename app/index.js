47782f203758cd805648355df33d7ed41564f704
Aelvish Patel <aelvishpatel@Aelvishs-MacBook-Pro.local>
2026-04-13 16:55:54 +0530

    Updated app/index.js

--- app/index.js
+++ app/index.js
@@ -21,7 +21,7 @@ register.registerMetric(httpRequestCounter);
 // Routes
 app.get('/', (req, res) => {
   httpRequestCounter.inc();
-  res.send('🚀 DevOps Demo App Running!');
+  res.send('🚀 DevOpss Demo App Running!');
 });
 
 app.get('/health', (req, res) => {
